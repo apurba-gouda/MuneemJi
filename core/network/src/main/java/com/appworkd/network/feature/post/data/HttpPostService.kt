e243556511d5601a5073d84e92fe3d9234c2c553
apurba-gouda <apurva.gouda@gmail.com>
2023-05-30 21:50:31 +0530

    Gradle Script changes for serialization[Kotlin].

--- core/network/src/main/java/com/appworkd/network/feature/post/data/HttpPostService.kt
+++ core/network/src/main/java/com/appworkd/network/feature/post/data/HttpPostService.kt
@@ -1,19 +1,14 @@
-@file:OptIn(ExperimentalSerializationApi::class)
-
 package com.appworkd.network.feature.post.data
 
 import com.appworkd.network.feature.post.PostService
 import com.appworkd.network.feature.post.model.Post
-import com.appworkd.network.feature.post.model.PostResponse
 import com.appworkd.network.ktor.httpCall
 import com.appworkd.network.model.exceptions.ApiException
 import com.appworkd.network.serde.JsonProvider
 import com.appworkd.network.serde.httpDeserialize
-
-import io.ktor.client.*
-import io.ktor.client.request.*
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
 import io.ktor.http.path
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.builtins.ListSerializer
 import javax.inject.Inject
 
